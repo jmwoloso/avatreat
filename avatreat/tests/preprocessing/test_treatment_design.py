e2cddb23b76809509c8b2e5b798f87a589e21b91
jason wolosonovich <jason@avaland.io>
2018-01-04 18:34:35 -0700

    TST: added very basic test suite for TreatmentDesign

---
+++ avatreat/tests/preprocessing/test_treatment_design.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class TestTreatmentDesign(unittest.TestCase):
+    """Test suite for the avatreat.preprocessing.TreatmentDesign
+    class."""
+    def setUp(self):
+        """Setup method for the test suite."""
+        pass
+
+    def tearDown(self):
+        """Tear down method for the test suite."""
+        pass
