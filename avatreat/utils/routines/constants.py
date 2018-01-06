e6dd358305103fac0efe87257c19c9966a773d8f
jason wolosonovich <jason@avaland.io>
2018-01-06 01:50:04 -0700

    CLN: added NUMERICAL_DTYPES constant

--- avatreat/utils/routines/constants.py
+++ avatreat/utils/routines/constants.py
@@ -5,3 +5,7 @@ INT_DTYPES = ["int_", "intc", "intp", "int8", "int16", "int32",
               "int64", "uint8", "uint16", "uint32", "uint64"]
 
 FLOAT_DTYPES = ["float_", "float16", "float32", "float64"]
+
+NUMERICAL_DTYPES = ["int_", "intc", "intp", "int8", "int16", "int32",
+                    "int64", "uint8", "uint16", "uint32", "uint64",
+                    "float_", "float16", "float32", "float64"]
