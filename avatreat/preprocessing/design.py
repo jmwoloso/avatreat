dcb234651e38ce0dce6ad9e9ea8d198a34699378
jason wolosonovich <jason@avaland.io>
2018-01-06 01:45:51 -0700

    ENH: added lots of new functionality for TreatmentDesign.fit method

--- avatreat/preprocessing/design.py
+++ avatreat/preprocessing/design.py
@@ -1,9 +1,11 @@
-from ..utils.routines import exclude_features, reindex_target, \
-    get_column_dtypes, get_high_cardinality_features
+from ..utils.routines.fit import find_hidden_dtypes, \
+    fill_missing_values, find_zero_variance_features, \
+    get_treatment_features, reindex_target, cast_to_int, \
+    get_column_dtypes, find_high_cardinality_features
 
 
 class TreatmentDesign(object):
-    def __init__(self, id_features=None,
+    def __init__(self, index_features=None,
                  datetime_features=None,
                  target=None, target_type="categorical",
                  missing_numerical_strategy="systematically",
@@ -12,28 +14,33 @@ class TreatmentDesign(object):
                  allowable_rare_percentage=0.1,
                  variable_significance_threshold="1/n",
                  smoothing_factor=0.0,
-                 dtype_compression=False):
+                 dtype_compression=False,
+                 exclude_zero_variance_features=True):
         """
         Class for designing data treatments.
 
         Parameters
         ----------
-        id_features :   one of {list of features, None}; default=None;
-        list of features that are purely for identification and will be
-        excluded from treatment design.
+        index_features:   one of {list of features, None};
+        default=None; list of features that are purely for
+        identification/indexing and will be excluded from treatment
+        design.
 
-        datetime_features   :   one of {list of features, None};
+        datetime_features:   one of {list of features, None};
         default=None; list of datetime-like features; will be
         excluded from treatment design.
 
-        target  :   one of {str, None}; default=None; name of the
+        NOTE: treatment design will fail if the dataset contains
+        datetime features but none are specified.
+
+        target:   one of {str, None}; default=None; name of the
         target feature in the dataset.
 
-        target_type :   one of {"categorical", "numerical"};
+        target_type:   one of {"categorical", "numerical"};
         default="categorical"; whether the design is for a regression or
         classification target.
 
-        missing_numerical_strategy  :   one of {"systematically",
+        missing_numerical_strategy:   one of {"systematically",
         "random"}; default="systematically"; the strategy to employ
         when filling in missing numerical values; used in conjunction
         with `numerical_fill_value`; if strategy is set to "random" then
@@ -43,20 +50,22 @@ class TreatmentDesign(object):
         NOTE: the default strategy of "systematically" is the safe
         choice as importance is often implicit in missing values.
 
-        numerical_fill_value    :   one of {float, "mean"};
+        numerical_fill_value:   one of {float, "mean"};
         default=-1.0; value to fill in missing numerical rows with;
-        used in conjunction with `missing_numerical_strategy`.
+        used in conjunction with `missing_numerical_strategy`; will
+        be converted to np.int when attempting to fill missing int
+        values.
 
-        ints_to_categorical :   bool; default=True; whether to
+        ints_to_categorical:   bool; default=True; whether to
         attempt converting numeric features with dtype=int to
         categorical features; if True, rules applying to categorical
         features (`categorical_fill_value`, `rare_level_threshold`,
         `allowable_rare_percentage`) will be similarly applied.
 
-        categorical_fill_value  :   str; default="NA"; string to be
+        categorical_fill_value:   str; default="NA"; string to be
         used to fill in missing categorical levels.
 
-        rare_level_threshold    :   float; default=0.02; the
+        rare_level_threshold:   float; default=0.02; the
         percetage of the dataset that a categorical level must not
         exceed in order to be classified as (and treated accordingly) a
         rare level.
@@ -64,7 +73,7 @@ class TreatmentDesign(object):
         NOTE: rare levels will be pooled together when creating
         indicators for categorical features.
 
-        allowable_rare_percentage   :   float; default=0.1; total
+        allowable_rare_percentage:   float; default=0.1; total
         percentage of the dataset that all "rare" levels of a
         categorical feature must not exceed in order to pool rare
         levels; aims to prevent categorical features with
@@ -73,32 +82,60 @@ class TreatmentDesign(object):
         exceeding this level will be classified as having
         high-cardinality and will not have indicators created.
 
-        variable_significance_threshold :   one of {"1/n", float,
+        variable_significance_threshold:   one of {"1/n", float,
         None}; default="1/n" features; significance level that a
         feature must achieve in order to remain in the final treated
         dataset; if `None` all features will be kept.
 
-        smoothing_factor    :   float; default=0.0; number of
+        smoothing_factor:   float; default=0.0; number of
         pseudo-observations to add as a Laplace smoothing factor;
         reduces the range of predictions of rare levels.
 
-        dtype_compression   :   bool; default=True; whether to
+        dtype_compression:   bool; default=True; whether to
         downcast column dtypes (e.g. np.int64 -> np.int8);
         downcasting depends on the range of values present for a
         given column and may not always be possible/successful;
         useful for very large datasets as it helps reduce the
         in-memory footprint of the data.
 
+        exclude_zero_variance_features: bool; default=True; whether
+        to eliminate features with no variance from treatment design
+        as they contain no useful information.
+
 
         Attributes
         ----------
-        self.df_    :   pandas.DataFrame instance with ID and
-        datetime features removed.
+        df_:   copy of the supplied dataframe used for modification
+
+        blacklist_: features to be excluded from treatment design due
+        to having zero-variance.
+
+        treatment_features_:   features to be used for treatment design.
+
+        int_features_:   integer features of the dataframe.
+
+        float_features_:   float features of the dataframe.
+
+        object_features_:   object/str features of the dataframe.
+
+        bool_features_: boolean features of the dataframe.
+
+        high_cardinality_features_: features considered to be
+        high-cardinality features (high relative number of category
+        levels).
+
+        categorical_features_:  features that can be safely converted to
+        the categorical dtype.
+
+
+        Returns
+        -------
+        self    :   object
 
         """
         # if no id features are supplied, replace None with empty list
-        self.id_features = list(id_features) \
-            if id_features is not None \
+        self.index_features = list(index_features) \
+            if index_features is not None \
             else list()
         # if no datetime features are supplied, replace None with
         # empty list
@@ -117,6 +154,7 @@ class TreatmentDesign(object):
         self.variable_significance_threshold = variable_significance_threshold
         self.smoothing_factor = smoothing_factor
         self.dtype_compression = dtype_compression
+        self.exclude_zero_variance_features = exclude_zero_variance_features
 
 
     def fit(self, DF):
@@ -128,30 +166,60 @@ class TreatmentDesign(object):
         DF   :   pandas.DataFrame instance; dataframe used to to
         design treatments.
 
-
-        Returns
-        -------
-        self    :   object
         """
-        # remove the id and datetime features
-        self.df_ = exclude_features(dataframe=DF,
-                                    id_features=self.id_features,
-                                    datetime_features=self.datetime_features)
+        # copy the dataframe for modification
+        self.df_ = DF.copy()
+
+        # find any hidden dtypes within the object dtypes and
+        # downcast numerical features if specified
+        self.df_ = find_hidden_dtypes(dataframe=self.df_)
+
+        # fill in missing values
+        self.df_ = fill_missing_values(dataframe=self.df_,
+                                       numerical_fill_value=self.numerical_fill_value,
+                                       categorical_fill_value=self.categorical_fill_value)
+
+        self.blacklist_ = \
+            find_zero_variance_features(dataframe=self.df_,
+            exclude_zero_variance_features=self.exclude_zero_variance_features,
+                                        categorical_fill_value=self.categorical_fill_value)
+
+        # get a list of the features to be included in treatment
+        # design; also removes zero-variance features
+        self.treatment_features_ = get_treatment_features(
+            dataframe=self.df_,
+            id_features=self.index_features,
+            datetime_features=self.datetime_features,
+            target=self.target,
+            blacklist=self.blacklist_)
 
         # move the target feature to the end of the dataframe
         self.df_ = reindex_target(dataframe=self.df_,
                                   target=self.target)
 
+        # try and cast float features to ints
+        self.df_.loc[:, self.treatment_features_] = \
+            cast_to_int(dataframe=self.df_,
+                        treatment_features=self.treatment_features_)
+
+        # inspect object features for booleans
+        # objs = self.df_.select_dtypes(include=["object"]).columns
+
+
         # get the features by dtype
-        self.int_features_, self.float_features_, self.object_features_ = \
-            get_column_dtypes(dataframe=self.df_)
+        self.int_features_, \
+        self.float_features_, \
+        self.object_features_, \
+        self.bool_features_ = \
+            get_column_dtypes(dataframe=self.df_,
+                              treatment_features=self.treatment_features_)
 
         # find high-cardinality features from within object features
         self.high_cardinality_features_, self.categorical_features_ = \
-            get_high_cardinality_features(dataframe=self.df_,
-                                          object_features=self.object_features_,
-                                          rare_level_threshold=0.02,
-                                          allowable_rare_percentage=0.1)
+            find_high_cardinality_features(dataframe=self.df_,
+                                           object_features=self.object_features_,
+                                           rare_level_threshold=0.02,
+                                           allowable_rare_percentage=0.1)
 
 
 
