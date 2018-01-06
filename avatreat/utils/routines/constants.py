6867c27a5e2ec23cda14e913f6caf36b8c87e7e7
jason wolosonovich <jason@avaland.io>
2018-01-05 22:27:24 -0700

    CLN: added constants.py to routines for commonly used objects

---
+++ avatreat/utils/routines/constants.py
@@ -0,0 +1,7 @@
+
+
+
+INT_DTYPES = ["int_", "intc", "intp", "int8", "int16", "int32",
+              "int64", "uint8", "uint16", "uint32", "uint64"]
+
+FLOAT_DTYPES = ["float_", "float16", "float32", "float64"]
