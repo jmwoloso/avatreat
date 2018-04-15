6b4eddf4cd17b672f4a2de36c286b506aec1567e
jason wolosonovich <jason@avaland.io>
2018-04-15 13:30:14 -0700

    ENH: added constants for datetime-like dtypes

--- avatreat/utils/constants.py
+++ avatreat/utils/constants.py
@@ -9,3 +9,11 @@ FLOAT_DTYPES = ["float_", "float16", "float32", "float64"]
 NUMERICAL_DTYPES = ["int_", "intc", "intp", "int8", "int16", "int32",
                     "int64", "uint8", "uint16", "uint32", "uint64",
                     "float_", "float16", "float32", "float64"]
+
+DATETIME_DTYPES = ["datetime", "datetime64"]
+
+TIMEDELTAS = ["timedelta", "timedelta64"]
+
+CATEGORICAL_DTYPES = ["category"]
+
+DATETIMETZ_DTYPES = ["datetimetz"]
